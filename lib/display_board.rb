223c71fac8e5501d2e252842b6c858efbc43b25c
Yan Kozlovskiy <yankozlovskiy@icloud.com>
2017-06-26 06:54:48 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(arr)
+  board = ""
+  arr.length.times do |i|
+    if i == 0 || i == 2 || i == 3 || i == 5 || i == 6 || i == 8
+      board += " #{arr[i]} "
+    end
+    if i == 1 || i == 4 || i == 7
+      board += "| #{arr[i]} |"
+    end
+    if i == 2 || i == 5
+      board += "\n-----------\n"
+    end
+  end
+  puts board
+end
